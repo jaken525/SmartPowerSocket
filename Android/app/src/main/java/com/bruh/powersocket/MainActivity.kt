8dea5ed1a8a0e43102a45c48323bbfaaabddc977
Rostislav Zyryanov <rost0406s2004@yandex.ru>
2025-12-21 17:20:26 +0500

    Добавление сетевого слоя

--- Android/app/src/main/java/com/bruh/powersocket/MainActivity.kt
+++ Android/app/src/main/java/com/bruh/powersocket/MainActivity.kt
@@ -5,12 +5,23 @@ import android.widget.Button
 import android.widget.TextView
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.bruh.powersocket.network.ApiClient
+import com.bruh.powersocket.network.ApiService
+import com.bruh.powersocket.network.models.PowerResponse
+import com.bruh.powersocket.network.models.StatusResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var api: ApiService  // Добавлено поле
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        api = ApiClient.retrofit.create(ApiService::class.java)  // Добавлена инициализация
 
         val powerButton = findViewById<Button>(R.id.powerButton)
         val statsButton = findViewById<Button>(R.id.statsButton)
